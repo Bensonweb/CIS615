605a4edaaf7321712eccca89096e89308d421452
Bensonweb <1733918@park.edu>
2026-03-21 23:37:25 -0500

    Add files via upload

---
+++ GradeEstimator_1733918.py
@@ -0,0 +1,14 @@
+# This program asks the user for their first and last name
+# and then prints the full name
+
+# Ask the user to enter their first name and store it in a variable
+first_name = input("Enter your first name: ")
+
+# Ask the user to enter their last name and store it in another variable
+last_name = input("Enter your last name: ")
+
+# Combine the first name and last name with a space in between
+full_name = first_name + " " + last_name
+
+# Print the full name to the screen
+print(full_name) 
